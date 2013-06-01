56d3db6c324e95a6409a08092647021496961a59
John Potocny <Potocnyj@gmail.com>
2013-06-01 15:39:43 -0400

    Problem 4 Initial Functions
    Added function to check if a number is a palindrome. Now I just need a
    function to find all of the palindromes made from the product of two 3
    digit numbers...

--- Problem 4/Problem4.hs
+++ Problem 4/Problem4.hs
@@ -1,68 +1,23 @@
 -- ProjectEuler.net Solutions
--- Problem 3: What is the largest 
--- prime factor of the number 600851475143?
-
--- Function: largestPrimeFactor
--- Inputs: Integer n
--- Returns: Integer
--- largestPrimeFactor finds the largest 
--- prime factor of the input integer.
-largestPrimeFactor :: Integer -> Integer
-
-largestPrimeFactor n = foldr max 0 (primeFactors n)
+-- Problem 4: Find the largest palindrome
+-- made from the product of two 3-digit numbers.
 
 
--- Function: primeFactors
--- Inputs: Integer x
--- Returns: list of Integers
--- primeFactors builds a list of the prime factors 
--- of the input integer x.
-primeFactors :: Integer -> [Integer]
-
-primeFactors x = filter prime (divisors x)
-
--- Function prime
--- Input: integer n
--- Returns: Boolean
--- prime takes in an integer value as input,
--- and uses the definition of a prime number
--- to determine if it is prime. If it is prime,
--- it returns true, otherwise it returns false
-prime :: Integer -> Bool
-
-prime n
-	| n == 1				= False
-	| (divisors n) == [n,1]	= True
+-- Function: isPalindrome
+-- Input: List of Integers
+-- Returns: Bool
+-- isPalindrome takes a list of digits that make up a number, 
+-- and checks to see if the number's digits make it a palindrome
+isPalindrome :: [Integer] -> Bool
+isPalindrome (x:xs)
+	| length xs == 0 		= True
+	| (length xs) == 1 		= (x == head xs)
+	| (x == last xs) 		= (isPalindrome (init xs))
 	| otherwise 			= False
 
-
--- Function divisors
--- Input: integer n
--- Returns: List of integers
--- divisors looks at an input number, and 
--- checks every number from 1 to the input number value to 
--- determine if it is a divisor of the input. All divisors of the 
--- input are returned in a list
-divisors :: Integer -> [Integer]
-divisors n = n : [x | x <- [1, 2 .. (root n)], mod n x == 0]
-
-
--- Function root
--- Input: Integer [Integral] n
--- Returns: Integral of same type input
--- root will calculate the square root of the input number, 
--- and then return it as an Integral type
-root :: Integral n => n -> n
-root =  floor . sqrt . fromIntegral
-
--- A sieve filter for primes
-sieve :: [Integer] -> [Integer]
-sieve  (x:xs) = x : (sieve [y | y <- xs, mod y x > 0])
-
--- Infinite list of primes!
-primes :: [Integer]
-primes = sieve [2 .. ]
-
--- All of the primes, up to the input number
-primesTo :: Integer -> [Integer]
-primesTo x = sieve [2 .. x]
+-- Function: digits
+-- Input: Integer
+-- Returns: List of Integers
+-- digits will break an Integer into an ordered list of its digits
+digits :: Integer -> [Integer]
+digits n = map (`mod` 10) $ reverse $ takeWhile (> 0) $ iterate (`div` 10) n
